62158c038df674fb863e64eb837f1a5e37344556
bgst009 <1499135121@qq.com>
2022-07-16 17:48:23 +0800

    use tool.Plus

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/bgst009/urban-robot
+
+go 1.18
+
+require github.com/bgst009/miniature-octo-guide v0.0.0-20220716085105-37a0575edfc2 // indirect
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/bgst009/miniature-octo-guide/tool"
+)
+
+func main() {
+	fmt.Println("Hello world!")
+	fmt.Printf("tool.Plus(1, 2): %v\n", tool.Plus(1, 2))
+}
